41066d14ab8d2e9db0ca9ec873c69419ca496668
trashcognito <denizmersinlioglu2005@gmail.com>
2021-07-11 00:12:32 +0300

    added stuff

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,12 @@ fn pause() {
     // Read a single byte and discard
     let _ = stdin.read(&mut [0u8]).expect("Could not read string from stdin!");
 }
-struct RLE {
+struct RLE<const W: usize, const H: usize> {
     rules_survival: [bool; 9],
     rules_birth: [bool; 9],
-    map: [[bool; 16]; 16]
+    map: [[bool; W]; H]
 }
-impl RLE {
+impl<const W:usize, const H:usize> RLE<W, H> {
     pub fn new(filename: &str) -> Self {
         let mut raw_data = String::new();
         {
@@ -29,11 +29,12 @@ impl RLE {
                 f.read_to_string(&mut raw_data)
                     .expect("Could not read string from file!");
         };
-        let mut map = [[false; 16]; 16];
+        let mut map = [[false; W]; H];
         let mut rules_birth = [false; 9];
         let mut rules_survival = [false; 9];
         let mut rules_touched = false;
         let mut first_line = true;
+        let mut x_offset = 0;
         let mut x = 0;
         let mut y = 0;
         let mut repeat = 0;
@@ -70,6 +71,23 @@ impl RLE {
                                 }
                             }
                         }
+                        "x" => {
+                            let num = rhs.parse::<u32>()
+                                .expect("Invalid x parameter!");
+                            if num > W as u32 {
+                                panic!("Pattern too wide for program!");
+                            }
+                            x_offset = ((W / 2) - num as usize);
+                            x = x_offset;
+                        }
+                        "y" => {
+                            let num = rhs.parse::<u32>()
+                                .expect("Invalid y parameter!");
+                            if num > W as u32{
+                                panic!("Pattern too long for program!");
+                            }
+                            y = ((H / 2) - num as usize);
+                        }
                         _ => {
 
                         }
@@ -100,14 +118,17 @@ impl RLE {
                                     match char {
                                         'b' => {
                                             x += 1;
+                                            x %= W;
                                         }
                                         'o' => {
                                             map[y][x] = true;
                                             x += 1;
+                                            x %= W;
                                         }
                                         '$' => {
-                                            x = 0;
+                                            x = x_offset;
                                             y += 1;
+                                            y %= H;
                                         }
                                         _ => {}
                                     }
@@ -132,7 +153,7 @@ impl RLE {
     }
     pub fn do_iteration(&mut self) {
         self.print_grid();
-        let mut return_value = [[false;16];16];
+        let mut return_value = [[false;W];H];
         let offsets = [
             [-1 as isize,1],
             [-1,0],
@@ -143,11 +164,11 @@ impl RLE {
             [1,0],
             [1,-1]
         ];
-        for x in 0..16 {
-            for y in 0..16 {
+        for x in 0..W {
+            for y in 0..H {
                 let mut neighs = 0;
                 for offset in offsets {
-                    if self.map[((y + offset[0] + 16) % 16) as usize][((x + offset[1] + 16) % 16) as usize] {
+                    if self.map[((y as isize + offset[0] + H as isize) as usize % H) as usize][((x as isize + offset[1] + W as isize) as usize % W) as usize] {
                         neighs += 1;
                     }
                 }
@@ -166,8 +187,8 @@ impl RLE {
     fn print_grid(&self) {
         pause();
         print!("\x1B[2J\x1B[1;1H"); // clear the screen
-        for y in 0..16 {
-            for x in 0..16 {
+        for y in 0..H {
+            for x in 0..W {
                 print!("{}", match self.map[y][x] {
                     true => "*",
                     false => " "
@@ -187,7 +208,7 @@ fn main() {
         ).get_matches();
     let filename = cfg.value_of("file")
         .expect("No filename found (somehow!)");
-    let mut rle = RLE::new(filename);
+    let mut rle = RLE::<16,16>::new(filename);
     loop {
         rle.do_iteration();
     }
